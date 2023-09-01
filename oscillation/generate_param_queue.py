098567f7a983521a9cbfcf9b851c60fde29959bf
pranav-bhamidipati <pbhamidi@usc.edu>
2023-09-01 18:27:41 +0000

    5tf param table

--- oscillation/generate_param_queue.py
+++ oscillation/generate_param_queue.py
@@ -1,4 +1,3 @@
-from typing import Optional
 import numpy as np
 from pathlib import Path
 import pandas as pd
@@ -9,16 +8,16 @@ from gillespie import (
     PARAM_NAMES,
     MEAN_INITIAL_POPULATION,
     convert_uniform_to_params,
-    draw_random_initial_protein,
 )
 
 
 def main(
     n_samples: int,
     save_dir: Path,
-    n_components: int = 3,
+    n_components: int,
     seed: int = 2023,
     save: bool = False,
+    suffix: str = "",
 ):
     n_params = len(SAMPLING_RANGES)
     rg = np.random.default_rng(seed)
@@ -27,7 +26,9 @@ def main(
     param_sets = np.array(
         [convert_uniform_to_params(u, SAMPLING_RANGES) for u in uniform_samples]
     )
-    initial_conditions = rg.poisson(MEAN_INITIAL_POPULATION, size=(n_samples, n_components))
+    initial_conditions = rg.poisson(
+        MEAN_INITIAL_POPULATION, size=(n_samples, n_components)
+    )
     init_columns = [f"{chr(ord('A') + i)}_0" for i in range(n_components)]
     init_data = pd.DataFrame(initial_conditions, columns=init_columns)
     init_data.index.name = "sample_num"
@@ -37,7 +38,7 @@ def main(
     ...
 
     if save:
-        fname = Path(save_dir) / f"param_sets_queue_{n_samples}.csv"
+        fname = Path(save_dir) / f"param_sets_queue_{n_samples}{suffix}.csv"
         fname = fname.resolve().absolute()
         print(f"Writing parameter queue to {fname}")
         data.to_csv(fname)
@@ -47,7 +48,9 @@ if __name__ == "__main__":
     save_dir = Path("data/oscillation")
     save_dir.mkdir(exist_ok=True)
     main(
-        10000,
-        save_dir,
+        n_samples=10_000,
+        n_components=5,
+        save_dir=save_dir,
         save=True,
+        suffix="_5tf",
     )
