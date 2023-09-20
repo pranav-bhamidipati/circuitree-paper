8903a85c201e7cbdd9d2ee5a583134529b5a0935
pranav-bhamidipati <pbhamidi@usc.edu>
2023-09-20 00:58:37 +0000

    clarify rng seeds vs visit #s; ssa w/o time limit

--- oscillation/oscillation_local_celery.py
+++ oscillation/oscillation_local_celery.py
@@ -71,6 +71,8 @@ from tf_network import TFNetworkModel
 # "socket_keepalive": True,
 # }
 
+INT64_MAXVAL = np.iinfo(np.int64).max
+
 app = Celery("tasks")
 app.config_from_object("celeryconfig")
 task_logger = get_task_logger(__name__)
@@ -82,6 +84,7 @@ nt = 2000
 max_iter_per_timestep = 100_000_000
 nchunks = 5
 autocorr_threshold = 0.4
+
 save_dir = (
     Path("~/git/circuitree-paper/data/oscillation/mcts/230908_3tf_localredis")
     .expanduser()
@@ -89,15 +92,62 @@ save_dir = (
 )
 
 
+@app.task(queue="cleanup", ignore_result=True)
+def run_ssa_nolimit(
+    state: str,
+    seed: int,
+):
+    """Run SSA without a time limit."""
+    task_logger.info(f"Received {seed=}, {state=}")
+    state_key = "state_" + state.strip("*")
+    existing_entry = database.hget(state_key, str(seed))
+    if existing_entry is not None:
+        autocorr_min, sim_time = json.loads(existing_entry)
+        if (autocorr_min <= 0.0) and (sim_time > 0.0):
+            task_logger.info(f"Entry already exists. Skipping {seed=}, {state=}")
+            return
+
+    # Use the parameter set corresponding to the random seed
+    prots0, params = json.loads(database.hget("parameter_table", str(seed)))
+    kwargs = dict(
+        seed=seed,
+        prots0=prots0,
+        params=params,
+        state=state,
+        dt=dt,
+        nt=nt,
+        max_iter_per_timestep=INT64_MAXVAL,
+        autocorr_threshold=autocorr_threshold,
+        save_dir=save_dir,
+    )
+
+    prots_t, autocorr_min, sim_time = _run_ssa(**kwargs)
+    sim_time = float(sim_time)
+    kwargs["sim_time"] = sim_time
+    kwargs["prots_t"] = prots_t.tolist()
+
+    autocorr_min = float(autocorr_min)
+    task_logger.info(
+        f"Finished in {sim_time:.4f} secs with autocorr. min: {autocorr_min:.4f}. "
+        "Entering data in database."
+    )
+    database.hset(state_key, str(seed), json.dumps([autocorr_min, sim_time]))
+
+    oscillated = -autocorr_min > autocorr_threshold
+    if oscillated:
+        task_logger.info("Oscillation detected, saving results.")
+        save_results.delay(prefix="osc_", autocorr_min=autocorr_min, **kwargs)
+    else:
+        task_logger.info("No oscillations detected.")
+
+
 @app.task(soft_time_limit=120, time_limit=300, queue="simulations")
 def run_ssa(
     state: str,
-    visit_num: int,
-    # prots0: list[int],
-    # params: list[float],
+    seed: int,
 ):
+    task_logger.info(f"Received {seed=}, {state=}")
     # Use the parameter set corresponding to the visit number
-    seed = visit_num
     prots0, params = json.loads(database.hget("parameter_table", str(seed)))
     kwargs = dict(
         seed=seed,
@@ -112,7 +162,6 @@ def run_ssa(
     )
 
     try:
-        task_logger.info(f"Running SSA with {seed=}, {state=}")
         prots_t, autocorr_min, sim_time = _run_ssa(**kwargs)
         sim_time = float(sim_time)
         kwargs["sim_time"] = sim_time
@@ -225,6 +274,7 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
         time_limit: int = 120,
         callback_every: int = 10,
         counter: Optional[Counter] = None,
+        n_param_sets: Optional[int] = None,
         **kwargs,
     ):
         super().__init__(*args, **kwargs)
@@ -235,6 +285,9 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
         self.iteration_counter = 0
         self.callback_every = callback_every
 
+        if n_param_sets is None:
+            self.n_param_sets: int = database.hlen("parameter_table")
+
         # Specify attributes that should not/cannot be serialized when dumping to file
         self._non_serializable_attrs.extend(
             ["visit_counter", "logger", "iteration_counter"]
@@ -242,7 +295,7 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
 
     def get_reward(self, state: str):
         visit0 = self.visit_counter[state]
-        visits = np.arange(visit0, visit0 + self.batch_size)
+        seeds = (visit0 + np.arange(self.batch_size)) % self.n_param_sets
 
         # Check if the state is in the transposition table and pull any rewards
         state_key = "state_" + state.strip("*")  # asterisk is interpreted as wildcard
@@ -252,7 +305,7 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
         autocorr_mins = -np.ones(self.batch_size, dtype=np.float64)
         which_to_simulate = np.ones(self.batch_size, dtype=bool)
         if state_in_ttable:
-            past_results = database.hmget(state_key, visits.tolist())
+            past_results = database.hmget(state_key, seeds.tolist())
             for i, res in enumerate(past_results):
                 in_table = res is not None
                 which_to_simulate[i] = not in_table
@@ -261,7 +314,6 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
                     # simulation returned NaN (maxiter exceeded or timeout)
                     autocorr_min, sim_time = json.loads(res)
                     if autocorr_min > 0.0:
-                        ...
                         autocorr_mins[i] = np.nan
 
         # Dispatch simulation tasks
@@ -269,46 +321,41 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
         new_autocorr_mins = np.ones(n_to_simulate, dtype=np.float64)
         new_sim_times = -np.ones(n_to_simulate, dtype=np.float64)
         group_result: AsyncResult | GroupResult = group(
-            run_ssa.s(state, int(visit)) for visit in visits[which_to_simulate]
+            run_ssa.s(state, int(seed)) for seed in seeds[which_to_simulate]
         ).apply_async()
         if n_to_simulate:
             try:
                 new_results = group_result.get(timeout=self.time_limit)
                 new_autocorr_mins[:], new_sim_times[:] = zip(*new_results)
             except Exception as e:
-                # get() timed out or task received hard timeout
-                if isinstance(e, (TimeoutError, TimeLimitExceeded)):
-                    results: Iterable[AsyncResult] = group_result.results
-                    for i, result in enumerate(results):
-                        if result.ready():
-                            _autocorr, _sim_time = result.get()
-                            if _autocorr <= 0:
-                                new_autocorr_mins[i] = _autocorr
-                                new_sim_times[i] = _sim_time
-                            else:
-                                # Simulation returned NaNs (maxiter_per_timestep exceeded)
-                                new_autocorr_mins[i] = np.nan
-                                new_sim_times[i] = np.nan
-                        else:
-                            new_autocorr_mins[i] = np.nan
-                            new_sim_times[i] = np.nan
-
-                    # Cancel any running tasks - SIGINT signal is equivalent to
-                    # KeyboardInterrupt, a softer signal than the default (SIGTERM)
-                    revoked = 0
-                    for result in results:
-                        if not result.ready():
-                            revoked += 1
-                            result.revoke(terminate=True, signal="SIGINT")
-                    if revoked:
-                        self.logger.info(
-                            f"Time limit exceeded, canceled {revoked} tasks."
-                        )
-                else:
+                if not isinstance(e, (TimeoutError, TimeLimitExceeded)):
                     raise
 
-            if np.isnan(new_autocorr_mins).any():
-                ...
+                # get() call timed out or task received hard timeout
+                results: Iterable[AsyncResult] = group_result.results
+                for i, result in enumerate(results):
+                    if result.ready():
+                        _autocorr, _sim_time = result.get()
+                        if _autocorr <= 0:
+                            new_autocorr_mins[i] = _autocorr
+                            new_sim_times[i] = _sim_time
+                        else:
+                            # Simulation returned NaNs (maxiter_per_timestep exceeded)
+                            new_autocorr_mins[i] = np.nan
+                            new_sim_times[i] = np.nan
+                    else:
+                        new_autocorr_mins[i] = np.nan
+                        new_sim_times[i] = np.nan
+
+                # Cancel any running tasks - SIGINT signal is equivalent to
+                # KeyboardInterrupt, a softer signal than the default (SIGTERM)
+                revoked = 0
+                for result in results:
+                    if not result.ready():
+                        revoked += 1
+                        result.revoke(terminate=True, signal="SIGINT")
+                if revoked:
+                    self.logger.info(f"Time limit exceeded, canceled {revoked} tasks.")
 
             # Add simulation results
             autocorr_mins[which_to_simulate] = new_autocorr_mins
@@ -319,15 +366,20 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
                 database.sadd("transposition_table_keys", state_key)
 
             result_mapping = {}
-            for v, a, t in zip(
-                visits[which_to_simulate], new_autocorr_mins, new_sim_times
+            for seed, acf_min, sim_time in zip(
+                seeds[which_to_simulate], new_autocorr_mins, new_sim_times
             ):
-                if database.hexists(state_key, str(v)):
+                if database.hexists(state_key, str(seed)):
                     continue
-                if np.isnan(a) or a > 0.0:
-                    a = 1.0
-                    t = -1.0
-                result_mapping[str(v)] = json.dumps([float(a), float(t)])
+                if np.isnan(acf_min) or acf_min > 0.0:
+                    acf_min = 1.0
+                    sim_time = -1.0
+
+                    # dispatch to cleanup queue - runs with no time limit
+                    run_ssa_nolimit.delay(state, int(seed))
+                result_mapping[str(seed)] = json.dumps(
+                    [float(acf_min), float(sim_time)]
+                )
             if result_mapping:
                 self.logger.info(f"Storing {len(result_mapping)} results in database.")
                 database.hset(state_key, mapping=result_mapping)
@@ -348,7 +400,7 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
 
         self.iteration_counter += 1
         if self.iteration_counter % self.callback_every == 0:
-            self._done_callback(self, state, visits.tolist(), autocorr_mins.tolist())
+            self._done_callback(self, state, seeds.tolist(), autocorr_mins.tolist())
         return reward
 
     @staticmethod
@@ -357,134 +409,3 @@ class OscillationTreeCeleryLocal(OscillationTree, ParallelTree):
 
     def add_done_callback(self, callback, *args, **kwargs):
         self._done_callback = partial(callback, *args, **kwargs)
-
-
-# def save_ttable_every_n_iters(tree: OscillationTreeParallel, *args, **kwargs):
-#     """Callback function to save the transposition table every n iterations."""
-#     tree.iteration_counter += 1
-#     tree.logger.info(f"Iteration {tree.iteration_counter} complete.")
-#     i = tree.iteration_counter
-#     if i % tree.save_ttable_every == 0:
-#         ttable_fpath = Path(tree.save_dir) / f"iter{i}_trans_table.csv"
-#         sim_table_fpath = Path(tree.save_dir) / f"iter{i}_simtime_table.csv"
-#         tree.logger.info(
-#             f"Saving transposition table and simulation times to {tree.save_dir}"
-#         )
-#         tree.ttable.to_csv(ttable_fpath, **kwargs)
-#         tree.simtime_table.to_csv(sim_table_fpath, **kwargs)
-
-
-# class OscillationTreeCeleryLocal(OscillationTreeParallel):
-#     def __init__(
-#         self,
-#         save_ttable_every: int = 10,
-#         sim_time_table: Optional[TranspositionTable] = None,
-#         logger: Any = None,
-#         time_limit: int = 120,
-#         *args,
-#         **kwargs,
-#     ):
-#         super().__init__(*args, **kwargs)
-
-#         self._simulation_time_table = TranspositionTable(sim_time_table)
-#         self.iteration_counter = 0
-#         self.save_ttable_every = save_ttable_every
-#         self.time_limit = time_limit
-
-#         if logger is None:
-#             self.logger = task_logger
-#         elif isinstance(logger, str) or isinstance(logger, Path):
-#             import logging
-
-#             self.logger = logging.getLogger(__name__)
-#             self.logger.handlers.clear()
-#             self.logger.setLevel(logging.INFO)
-#             log_fmt = logging.Formatter(
-#                 "%(asctime)s - %(name)s - %(levelname)s - %(message)s"
-#             )
-#             logfile = Path(logger)
-#             fh = logging.FileHandler(logfile, mode="w")
-#             fh.setFormatter(log_fmt)
-#             self.logger.addHandler(fh)
-#         else:
-#             self.logger = logger
-
-#         self.add_done_callback(save_ttable_every_n_iters)
-
-#         # Specify any attributes that should not be serialized when dumping to file
-#         self._non_serializable_attrs.extend(["_simulation_time_table", "logger"])
-
-#     @property
-#     def run_task(self) -> Task:
-#         return run_ssa
-
-#     @property
-#     def simtime_table(self):
-#         return self._simulation_time_table
-
-#     def save_results(self, data: dict[str, Any]) -> None:
-#         """Results are saved in the worker processes, so this method is a no-op."""
-#         return
-
-#     def compute_tasks_and_handle_errors(
-#         self, iter_args, n_retries: int = 5, **kwargs
-#     ) -> tuple[list[float], list[float]]:
-#         task_group = group(self.run_task.s(*args, **kwargs) for args in iter_args)
-#         group_result: GroupResult | AsyncResult = task_group.apply_async(
-#             retry=True, retry_policy={"max_retries": n_retries}
-#         )
-#         try:
-#             results = group_result.get(timeout=self.time_limit)
-#             rewards, sim_times = zip(*results)
-#         except Exception as e:
-#             # get() timed out or task received hard timeout
-#             if isinstance(e, (TimeoutError, TimeLimitExceeded)):
-#                 rewards = []
-#                 sim_times = []
-#                 revoked = 0
-#                 results: Iterable[AsyncResult] = group_result.results
-#                 for result in results:
-#                     if result.ready():
-#                         reward, sim_time = result.get()
-#                         r = reward if reward >= 0 else np.nan
-#                         s = sim_time if reward >= 0 else np.nan
-#                     else:
-#                         # Cancel any running tasks - SIGINT signal is equivalent to
-#                         # KeyboardInterrupt and is a softer terminate signal than the
-#                         # default (SIGTERM)
-#                         revoked += 1
-#                         result.revoke(terminate=True, signal="SIGINT")
-#                         r = np.nan
-#                         s = np.nan
-#                     rewards.append(r)
-#                     sim_times.append(s)
-#                 self.logger.info(f"Time limit exceeded, canceled {revoked} tasks.")
-
-#         return rewards, sim_times
-
-#     @cached_property
-#     def task_kwargs(self) -> dict[str, Any]:
-#         return dict(
-#             dt=float(self.dt),
-#             nt=int(self.nt),
-#             autocorr_threshold=float(self.autocorr_threshold),
-#             max_iter_per_timestep=int(self.max_iter_per_timestep),
-#             save_dir=str(self.save_dir),
-#         )
-
-#     def simulate_visits(self, state, visits) -> tuple[list[float], dict[str, Any]]:
-#         # Make the input args JSON serializable
-#         input_args = []
-#         for v in visits:
-#             seed, inits, params = self.param_table[v]
-#             input_args.append(
-#                 (int(seed), list(map(int, inits)), list(map(float, params)))
-#             )
-
-#         # Submit the tasks and wait for them to finish, handling timeouts and retries
-#         rewards, sim_times = self.compute_tasks_and_handle_errors(
-#             input_args, state=str(state), **self.task_kwargs
-#         )
-#         self.simtime_table[state].extend(list(sim_times))
-
-#         return rewards, {}
