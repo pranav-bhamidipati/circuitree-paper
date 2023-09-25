be9bcf69d0bb14346d6d1fffb9ae8d592a5adfcd
Pranav Bhamidipati <psbhamidipati@gmail.com>
2023-09-25 21:38:19 +0000

    use aws elasticache redis database

--- oscillation/celeryconfig.py
+++ oscillation/celeryconfig.py
@@ -1,20 +1,7 @@
-# import os
-# import ssl
+# broker_url = "redis://127.0.0.1:6379/0"
+broker_url = "redis://circuitree-cache2.t3c6zs.ng.0001.usw2.cache.amazonaws.com:6379"
 
-# _redis_url = os.environ.get("CELERY_BROKER_URL", "")
-# if _redis_url:
-#     broker_use_ssl = ssl.CERT_NONE
-#     redis_backend_use_ssl = {"ssl_cert_reqs": ssl.CERT_NONE}
-# else:
-#     # Some apps use redis:// url, no SSL needed
-#     _redis_url = os.environ["CELERY_BROKER_URL_INTERNAL"]
-
-# celery_broker_url = _redis_url
-# celery_result_backend = _redis_url
-
-broker_url = "redis://127.0.0.1:6379/0"
 result_backend = broker_url
-
 broker_connection_retry_on_startup = True
 worker_prefetch_multiplier = 1
 worker_cancel_long_running_tasks_on_connection_loss = True
