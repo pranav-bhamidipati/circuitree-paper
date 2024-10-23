2d4f404c03c650d3c1974c6bc409d802f11813f4
Pranav Bhamidipati <psbhamidipati@gmail.com>
2024-10-23 19:53:26 +0000

    revert valkey to redis, update db address

--- oscillation/celeryconfig.py
+++ oscillation/celeryconfig.py
@@ -1,5 +1,9 @@
-broker_url = "redis://127.0.0.1:6379/0"
+# broker_url = "redis://127.0.0.1:6379/0"
 # broker_url = "redis://circuitree-cache2.t3c6zs.ng.0001.usw2.cache.amazonaws.com:6379"
+# broker_url = "valkeys://pranav-circuitree-cache-t3c6zs.serverless.usw2.cache.amazonaws.com:6379"
+broker_url = (
+    "rediss://circuitree-cache-redis-t3c6zs.serverless.usw2.cache.amazonaws.com:6379"
+)
 
 result_backend = broker_url
 broker_connection_retry_on_startup = True
